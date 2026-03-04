0aa3281853a40f628d5db1dde0e283c722acf1ab
Lennin sabogal <lenninssp1021@gmail.com>
2026-03-04 12:14:06 -0500

    finished models

---
+++ src/app/core/enums/job-category.enum.ts
@@ -0,0 +1,7 @@
+// this one may change in the future because i dont know if this are the correct status
+
+export enum JobCategory {
+  TECH = "tech",
+  ADMINISTRATION = "administration",
+  PLUMBER = "plumber",
+}
